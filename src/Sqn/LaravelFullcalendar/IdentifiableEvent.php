9851808113a6b150afb766f0db3907cf21f8b2c9
Ash Radwan <ash.radwan@Ashs-MBP.lan>
2020-03-19 13:47:39 +0000

    calendar-module

---
+++ src/Sqn/LaravelFullcalendar/IdentifiableEvent.php
@@ -0,0 +1,13 @@
+<?php namespace Sqn\LaravelFullcalendar;
+
+interface IdentifiableEvent extends Event
+{
+
+    /**
+     * Get the event's ID
+     *
+     * @return int|string|null
+     */
+    public function getId();
+
+}
